1feead5acc6e98a601bcbf63ed4ce54ed852a2a9
DiegoAcetun <diegoacetun00345@gmail.com>
2021-12-14 12:45:46 -0600

    se hizo la funcion para cargar listas de reproduccion creadas

--- python/main.py
+++ python/main.py
@@ -364,7 +364,7 @@ def LeerXml():
     global ruta, listaAlbumes, listaCanciones, listaArtistas, ventana, var, listaC, listaVar
     tree = ET.parse("../archivos/1.xml")
     root = tree.getroot()
-    print(root)
+    
     for i in range(len(root)): #root.tag es biblioteca
         nombreCancion = (root[i].attrib) #Por cada iteracion guadar el nombre de cada cancion en un dicc
                                         #el tag es cancion
@@ -392,6 +392,208 @@ def LeerXml():
                 nuevaCancion.artista = root[i][j].text.strip(' ')
         
         listaCanciones.agregarFinal(nuevaCancion)
+    AnidarListas()
+    CrearCheckbox()
+    # aux = listaCanciones.primero #Esta es la primera lista aux cancion
+    # #Recorriendo la lista de canciones
+    # while aux != None:
+    #     if listaAlbumes.vacia():
+    #         listaAlbumAux = ListaDobleAlbum()
+    #         listaAlbumAux.agregarFinal(aux.dato)
+    #         listaAlbumAux.artista = aux.dato.artista
+    #         listaAlbumAux.album = aux.dato.album
+    #         # print(aux.dato.album, aux.dato.artista)
+            
+    #         listaAlbumes.agregarFinal(listaAlbumAux)
+
+    #         # print(aux.dato.nombre)
+    #         #seguir creando las listas album cuando ya hay elementos,
+    #         #recorrer la lista 
+    #         aux2 = listaAlbumes.primero
+
+    #     else:
+    #         albumEncontrado = False
+    #         # print(aux.dato.album, aux.dato.artista)
+    #         aux3 = listaAlbumes.primero
+    #         while aux3 != None:
+    #             # print(aux3.dato)
+    #             aux4=aux3.dato #aux4 tiene la lista album aux
+    #             # print(aux4.primero.dato.nombre) # Esto es el objeto cancion
+    #             # print(aux4.album)
+    #             aux3 = aux3.siguiente
+    #             if aux4.album == aux.dato.album: #Recorremos para ver si el album ya existe
+    #                 aux4.agregarFinal(aux.dato) #Le agregamos la cancion a ese album
+    #                 albumEncontrado = True
+    #                 break
+            
+    #         if albumEncontrado:
+    #             pass
+    #         else:
+    #             listaAlbumAux = ListaDobleAlbum()
+    #             listaAlbumAux.agregarFinal(aux.dato)
+    #             listaAlbumAux.artista = aux.dato.artista
+    #             listaAlbumAux.album = aux.dato.album
+    #             # print(listaAlbumAux.primero.dato.nombre) Con esto se accede a los atributos de la lista
+    #             #de canciones desde la lista aux album
+    #             listaAlbumes.agregarFinal(listaAlbumAux)
+
+    #             # print(aux.dato.nombre)
+
+
+
+    #     #print(aux.dato.ruta)#asi se accede a los atrubutos desde el auxiliar
+    #     aux=aux.siguiente
+
+    # #Recorriendo listas albumes
+    # aux = listaAlbumes.primero
+
+
+    # #Aqui solo se obtienen datos, no se crea nada
+    # while aux!=None:
+    #     aux2 = aux.dato #Esto es la lista album auxiliar
+    #     aux3 = aux.dato.primero
+    #     # print('*'*25)
+    #     # print('Canciones del album', aux.dato.album, 'artista', aux.dato.artista) #Aqui se imprime la info
+    #     #Descomentar para ver
+    #     while aux3 !=None:
+    #         # print(aux3.dato.nombre) descomentar para obtener atrib de la cancion
+    #         aux3=aux3.siguiente
+
+    #     # print(aux2.primero.dato.nombre) #Imprimo nombre de la cancion
+    #     aux = aux.siguiente
+
+
+    # #Aqui se anidan listasArtista y listaALbum
+    # aux = listaAlbumes.primero
+    # # print(aux.dato) #esto es un album auxiliar
+    # artistaEncontrado = False
+    # while aux != None: #Aqui recorro los 5 albumes
+    #     aux2 = aux.dato #Aqui estan los atributos de cada album
+    #     aux2 = aux2.artista
+    #     # print(aux.dato.size)
+    #     if listaArtistas.vacia():
+    #         listaAuxArtistas = ListaDobleArtista()
+    #         listaAuxArtistas.agregarFinal(aux.dato) #aux dato contiene el album a agregar
+    #         listaAuxArtistas.artista = aux2
+    #         listaArtistas.agregarFinal(listaAuxArtistas)
+    #         aux3 = listaArtistas.primero
+
+    #     else:
+    #         #aqui recorro las listas artistas
+    #         aux3 = listaArtistas.primero
+    #         while aux3 != None:
+    #             # print(aux2+aux3.dato.artista+'valores') #esta es una lista artista con atributos
+    #             #Verificamos si ya existe el artista y solo le agregamos el album
+    #             #aux 3 es lista artista
+    #             # print(aux2 == aux3.dato.artista, 'comparacion')
+    #             if aux2 == aux3.dato.artista:
+        
+    #                 aux3.dato.agregarFinal(aux.dato)
+
+    #                 artistaEncontrado = True
+                    
+
+    #                 # print('iguales', aux2)
+    #                 break
+    #             aux3 = aux3.siguiente
+                    
+                
+    #         #Si el artista no existe creamos una nueva instancia
+    #         if artistaEncontrado == False:
+    #             # print('no existe')
+    #             listaAuxArtistas = ListaDobleArtista()
+    #             listaAuxArtistas.agregarFinal(aux.dato) #aux dato contiene el album a agregar
+    #             listaAuxArtistas.artista = aux2
+    #             listaArtistas.agregarFinal(listaAuxArtistas)
+    #         artistaEncontrado = False
+                    
+    #     # print('sxsx')
+                
+
+
+            
+        
+
+    #     aux = aux.siguiente
+
+    # #Verificando los datos en la lista artista
+    # aux = listaArtistas.primero
+    # # print(listaArtistas.size)
+    # while aux != None:
+    #     # print('*'*25)
+    #     # print('artista', aux.dato.artista, )
+    #     # print(aux.dato.primero.dato)
+    #     aux2 = aux.dato.primero 
+    #     # aux4 = aux.dato.primero
+    #     # print('albumes', end='   ')
+    #     #Recorriendo albumes de los artistas para obtener solo los albumes
+    #     # while aux4 != None:
+            
+    #     #     print(aux4.dato.album, end='   ')
+    #     #     aux4 = aux4.siguiente
+
+
+    #     #Recorremos los albumes del artista para obtener las canciones
+        
+    #     while aux2 != None:
+            
+    #         # print('album', aux2.dato.album)
+    #         aux3 = aux2.dato.primero
+    #         #Recorriendo el album para pbtener las canciones
+    #         # print('CANCIONES')
+    #         while aux3 != None:
+    #             # print(aux3.dato.nombre)
+    #             aux3 = aux3.siguiente
+
+    #         aux2 = aux2.siguiente
+    #         # print(aux2)
+        
+
+
+        
+    #     aux = aux.siguiente
+
+    # aux = listaCanciones.primero
+    # posy = 0
+    # #Creamos los checkbox de las canciones
+    # while aux != None:
+    #     var = tk.BooleanVar()
+    #     # var = aux.dato.nombre
+    #     opcionCancion = tk.Checkbutton(ventana, text=aux.dato.nombre, variable=var)
+    #     opcionCancion.pack
+    #     opcionCancion.place(x=20, y=160+posy)
+    #     listaAux = ListaDoble()
+    #     listaAux.agregarFinal(opcionCancion)
+    #     listaVar.agregarFinal(var)
+
+    #     listaC.agregarFinal(opcionCancion)
+    #     # print(var.get())
+        
+    #     posy+=20
+    #     aux = aux.siguiente
+def CrearCheckbox():
+    global ruta, listaAlbumes, listaCanciones, listaArtistas, ventana, var, listaC, listaVar
+    aux = listaCanciones.primero
+    posy = 0
+    #Creamos los checkbox de las canciones
+    while aux != None:
+        var = tk.BooleanVar()
+        # var = aux.dato.nombre
+        opcionCancion = tk.Checkbutton(ventana, text=aux.dato.nombre, variable=var)
+        opcionCancion.pack
+        opcionCancion.place(x=20, y=160+posy)
+        listaAux = ListaDoble()
+        listaAux.agregarFinal(opcionCancion)
+        listaVar.agregarFinal(var)
+
+        listaC.agregarFinal(opcionCancion)
+        # print(var.get())
+        
+        posy+=20
+        aux = aux.siguiente
+
+def AnidarListas():
+    global ruta, listaAlbumes, listaCanciones, listaArtistas, ventana, var, listaC, listaVar
 
     aux = listaCanciones.primero #Esta es la primera lista aux cancion
     #Recorriendo la lista de canciones
@@ -551,26 +753,6 @@ def LeerXml():
 
         
         aux = aux.siguiente
-
-    aux = listaCanciones.primero
-    posy = 0
-    #Creamos los checkbox de las canciones
-    while aux != None:
-        var = tk.BooleanVar()
-        # var = aux.dato.nombre
-        opcionCancion = tk.Checkbutton(ventana, text=aux.dato.nombre, variable=var)
-        opcionCancion.pack
-        opcionCancion.place(x=20, y=160+posy)
-        listaAux = ListaDoble()
-        listaAux.agregarFinal(opcionCancion)
-        listaVar.agregarFinal(var)
-
-        listaC.agregarFinal(opcionCancion)
-        # print(var.get())
-        
-        posy+=20
-        aux = aux.siguiente
-    
 def ReporteHtml():
     global listaActual, combo, listasCirculares
     contenido=''
@@ -662,7 +844,7 @@ def generarXml():
     titulo = 'listasReproduccion.xml'
     reporte = open(titulo, 'w')
     
-    contenido = '<?xml version="1.0" encoding="UTF-8"?>\n<ListasReproducción> \n'
+    contenido = '<?xml version="1.0" encoding="UTF-8"?>\n<ListasReproducción>\n'
     
     aux = listasCirculares.primero #Esta es la primera lista de reproduccion
     # print(aux.dato)
@@ -672,11 +854,11 @@ def generarXml():
     while aux !=None:
         aux2 = aux.dato.primero #Esta es la primera cancion
 
-        contenido+=f'   <Lista nombre={aux.dato.nombre}>\n'
+        contenido+=f'   <Lista nombre="{aux.dato.nombre}">\n'
         
         while aux2:
             
-            contenido+=f'   <cancion nombre={aux2.dato.nombre}>\n'
+            contenido+=f'   <cancion nombre="{aux2.dato.nombre}">\n'
             contenido+=f'       <artista> {aux2.dato.artista} </artista>\n'
             contenido+=f'       <album> {aux2.dato.album} </album>\n'
             contenido+=f'       <vecesReproducidas> {aux2.dato.reproducciones} </vecesReproducidas>\n'
@@ -692,7 +874,7 @@ def generarXml():
         contenido+='    </Lista>\n'
         aux = aux.siguiente
     
-    contenido+='</ListasReproduccion>\n'
+    contenido+='</ListasReproducción>\n'
     reporte.write(contenido)
     reporte.close()
     msj = 'Archivo xml generado'
@@ -700,10 +882,45 @@ def generarXml():
 
     
         
+def CargarMILista():
+    global ruta, listaAlbumes, listaCanciones, listaArtistas, ventana, var, listaC, listaVar
+    global listasCirculares, listasReproduccion, listasReproduccionBox, pila
+    tree = ET.parse("./listasReproduccion.xml")
+    root = tree.getroot()
+    for i in range (len(root)):
+        listasReproduccion = ListaCircular()
+        listasReproduccion.nombre = root[i].attrib["nombre"].strip(' ').replace('"',' ')
+        pilaAux = []
+        for j in range(len(root[i])):
+            
 
-    
+            nuevaCancion = Cancion()
+            
+            nuevaCancion.nombre = root[i][j].attrib["nombre"]
+            # print(nuevaCancion.nombre)
+            for k in range(len(root[i][j])):
+                if root[i][j][k].tag == 'artista':
+                    nuevaCancion.artista = root[i][j][k].text.strip(' ').replace('"', '')
+                elif root[i][j][k].tag == 'album':
+                    nuevaCancion.album = root[i][j][k].text.strip(' ').replace('"', '')
+                elif root[i][j][k].tag == 'vecesReproducidas':
+                    nuevaCancion.reproducciones = int(root[i][j][k].text.strip(' ').replace('"', ''))
+                elif root[i][j][k].tag == 'imagen':
+                    nuevaCancion.imagen = root[i][j][k].text.strip(' ').replace('"', '')
+                elif root[i][j][k].tag == 'ruta':
+                    nuevaCancion.ruta = root[i][j][k].text.strip(' ').replace('"', '')
+            # listaCanciones.agregarFinal(nuevaCancion)
+            listasReproduccion.agregarFinal(nuevaCancion)
+            pilaAux.append(nuevaCancion)
+
+        listasCirculares.agregarFinal(listasReproduccion)
+        listasReproduccionBox.append(listasReproduccion.nombre)
+        pila.append(pilaAux)
+        combo.configure(values=(listasReproduccionBox))
 
 
+    AnidarListas()
+    CrearCheckbox()
     
         
             
@@ -792,10 +1009,13 @@ Aleatorio.set(False)
 RedioButonNormal = tk.Radiobutton(ventana, text='Normal', value=False, font=fuente, variable=Aleatorio, bg='lavender', fg = 'black', activebackground="powderblue").place(x=920, y=850)
 RedioButonAleatorio = tk.Radiobutton(ventana, text='Aleatorio', value=True, font=fuente, variable=Aleatorio, bg='lavender', fg = 'black', activebackground="powderblue").place(x=1100, y=850)
 
-botonCargar = tk.Button(ventana, text="Exportar listas", command=generarXml, height=2, width=15, bg="midnightblue", fg="white", activebackground="powderblue", font=fuente)
-botonCargar.pack()
-botonCargar.place(x=700, y=10)
+botonGenerarXml = tk.Button(ventana, text="Exportar listas", command=generarXml, height=2, width=15, bg="midnightblue", fg="white", activebackground="powderblue", font=fuente)
+botonGenerarXml.pack()
+botonGenerarXml.place(x=700, y=10)
 
+botonCragarMiLista = tk.Button(ventana, text="Cargar Mis Listas", command=CargarMILista, height=2, width=15, bg="midnightblue", fg="white", activebackground="powderblue", font=fuente)
+botonCragarMiLista.pack()
+botonCragarMiLista.place(x=470, y=720)
 LeerXml()
 ventana.mainloop()
 # opcionCancion = tk.Radiobutton(ventana, text='cancion 1', value=1, variable=var)
